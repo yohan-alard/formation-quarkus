a040cc883d42577d449d68f0991680abf2c0dd1b
Lapin rose <yalard@gmail.com>
2025-12-23 23:11:57 +0100

    feature: section 5

--- src/test/java/org/agoncal/quarkus/starting/BookResourceTest.java
+++ src/test/java/org/agoncal/quarkus/starting/BookResourceTest.java
@@ -1,20 +1,48 @@
 package org.agoncal.quarkus.starting;
 
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.MediaType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpHeaders.ACCEPT;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class BookResourceTest {
+
     @Test
-    void testHelloEndpoint() {
+    void getAllBooks() {
         given()
-          .when().get("/api/books")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .header(ACCEPT, MediaType.APPLICATION_JSON)
+                .when().get("/api/books")
+                .then()
+                .statusCode(200)
+                .body("size()", is(2));
     }
 
+    @Test
+    void countAllBooks() {
+        given()
+                .header(ACCEPT, MediaType.TEXT_PLAIN)
+                .when().get("/api/books/count")
+                .then()
+                .statusCode(200)
+                .body(is("2"));
+    }
+
+    @Test
+    void getBook() {
+        given()
+                .header(ACCEPT, MediaType.APPLICATION_JSON)
+                .pathParam("id", 1)
+                .when().get("/api/books/{id}")
+                .then()
+                .statusCode(200)
+                .body("id", is(1))
+                .body("title", is("george"))
+                .body("author", is("georghe"))
+                .body("yearOfPublication", is(2004))
+                .body("genre", is("fantasy"));
+    }
 }
